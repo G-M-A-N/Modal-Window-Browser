9df7cc43c6aec6248d01c5c33c4ad6105eb4f811
G-M-A-N <gmanngesh@gmail.com>
2022-09-12 14:45:40 +0300

    changed class to .show-Modal from .openModal

--- starter/script.js
+++ starter/script.js
@@ -3,4 +3,5 @@
 const modal = document.querySelector('.modal');
 const overLay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
-const btnsOpenModal = document.querySelector('.openModal');
+const btnsOpenModal = document.querySelector('.show-Modal');
+console.log(btnsOpenModal);
