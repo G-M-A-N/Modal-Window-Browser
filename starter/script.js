0c7bc8da2bec230bb81e05e43ca697767aedff6a
G-M-A-N <gmanngesh@gmail.com>
2022-09-12 14:58:52 +0300

    log show-modal contents to console

--- starter/script.js
+++ starter/script.js
@@ -3,5 +3,13 @@
 const modal = document.querySelector('.modal');
 const overLay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
-const btnsOpenModal = document.querySelector('.show-Modal');
+const btnsOpenModal = document.querySelectorAll('.show-Modal');
 console.log(btnsOpenModal);
+
+//conflict arises when using query selector when there are many html elements with the same class name.DocumentQuery selctor will select the first one only.
+
+//in this case it is wise to use querySelectorAll I.E
+//const btnsOpenModal = document.querySelectorAll('.show-Modal');
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  console.log(btnsOpenModal[i].textContent);
