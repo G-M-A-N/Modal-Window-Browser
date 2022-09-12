0ed7fc122cf2edca9f287a447ba02ff8e335ef83
G-M-A-N <gmanngesh@gmail.com>
2022-09-12 18:31:13 +0300

    used classlist.contains check if modal has hidden

--- starter/script.js
+++ starter/script.js
@@ -30,8 +30,7 @@ overLay.addEventListener('click', closeModal);
 //the function does not have a () so that the function is not called immediately.
 
 document.addEventListener('keydown', function (gr) {
-  if (gr.key === 'Escape') {
-    closeModal;
-    console.log('Esc was pressed');
-  }
+  if (gr.key === 'Escape') 
+  if (!modal.classList.contains('hidden');
+  overLay.classList.add('hidden');
 });
